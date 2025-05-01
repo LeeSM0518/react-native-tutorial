59acb2e3bc722d5c799eacd86e1307b90b26e0b7
Sangmin Lee <nalsm98@naver.com>
2025-05-01 21:22:39 +0900

    chore: upload the files related to animation

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,17 +1,48 @@
-import {Text, StyleSheet, View} from "react-native";
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
+} from 'react-native-reanimated';
+import { View, Button, StyleSheet } from 'react-native';
 
 export default function SettingsScreen() {
+  const randomWidth = useSharedValue(10);
+
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    };
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Settings Screen</Text>
+      <Animated.View style={[styles.box, style]} />
+      <Button
+        title="toggle"
+        onPress={() => {
+          randomWidth.value = Math.random() * 350;
+        }}
+      />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-  }
+    justifyContent: 'center',
+  },
+  box: {
+    width: 100,
+    height: 80,
+    backgroundColor: 'black',
+    margin: 30,
+  },
 });

31c083e3fcb792ebfec38b9044b52693730c29d2
Sangmin Lee <nalsm98@naver.com>
2025-05-05 21:41:19 +0900

    chore: upload the files related to react native tutorial

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -4,14 +4,15 @@ import {Link} from "expo-router";
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <Link href="/(tabs)/(home)/core-components">Core Components</Link>
-      <Link href="/(tabs)/(home)/react-fundamentals">React Fundamentals</Link>
-      <Link href="/(tabs)/(home)/handling-text-input">Handling Text Input</Link>
-      <Link href="/(tabs)/(home)/using-scroll-view">Using a ScrollView</Link>
-      <Link href="/(tabs)/(home)/using-flat-list">Using FlatList</Link>
-      <Link href="/(tabs)/(home)/using-section-list">Using SectionList</Link>
-      <Link href="/(tabs)/(home)/platform-specific-code">Platform-Specific Code</Link>
-      <Link href="/(tabs)/(home)/core-component-style">style</Link>
+      <Link style={styles.link} href="/(tabs)/(home)/core-components">Core Components</Link>
+      <Link style={styles.link} href="/(tabs)/(home)/react-fundamentals">React Fundamentals</Link>
+      <Link style={styles.link} href="/(tabs)/(home)/handling-text-input">Handling Text Input</Link>
+      <Link style={styles.link} href="/(tabs)/(home)/using-scroll-view">Using a ScrollView</Link>
+      <Link style={styles.link} href="/(tabs)/(home)/using-flat-list">Using FlatList</Link>
+      <Link style={styles.link} href="/(tabs)/(home)/using-section-list">Using SectionList</Link>
+      <Link style={styles.link} href="/(tabs)/(home)/platform-specific-code">Platform-Specific Code</Link>
+      <Link style={styles.link} href="/(tabs)/(home)/core-component-style">style</Link>
+      <Link style={styles.link} href="/(tabs)/(home)/fixed-dimensions">Fixed Dimensions</Link>
     </SafeAreaView>
   );
 }
@@ -21,5 +22,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  link: {
+    fontSize: 20,
+    marginVertical: 10,
+    borderBottomWidth: 1,
+  },
 });

92315a5ffa62e3527470b5600c76af999204df39
Sangmin Lee <nalsm98@naver.com>
2025-05-05 17:11:08 +0900

    chore: upload the files related to react native tutorial

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -7,6 +7,7 @@ export default function HomeScreen() {
       <Link href="/(tabs)/(home)/core-components">Core Components</Link>
       <Link href="/(tabs)/(home)/react-fundamentals">React Fundamentals</Link>
       <Link href="/(tabs)/(home)/handling-text-input">Handling Text Input</Link>
+      <Link href="/(tabs)/(home)/using-scroll-view">Using a ScrollView</Link>
     </SafeAreaView>
   );
 }
